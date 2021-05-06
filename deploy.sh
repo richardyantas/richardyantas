2c7f076616cdfd8792ab34afaef8deeeb461ed10
richardyantas5 <richadyantas5@gmail.com>
2021-05-06 12:43:23 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 # make sure deploy on the main terminal not vscode terminal
 npm run deploy
-# export PUBLIC_URL=localhost:3000
 # export PUBLIC_URL=http://richardyantas.github.io/richardyantas
 # yarn build
 git add .
