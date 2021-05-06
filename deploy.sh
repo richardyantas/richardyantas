f8bb637f06895403cf0b5bb972e4840f7270a001
richardyantas5 <richadyantas5@gmail.com>
2021-05-06 11:30:44 -0500

    deploying ..

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
-
+# make sure deploy on the main terminal not vscode terminal
 npm run deploy
 # export PUBLIC_URL=localhost:3000
-export PUBLIC_URL=http://richardyantas.github.io/richardyantas
-yarn build
-  
+# export PUBLIC_URL=http://richardyantas.github.io/richardyantas
+# yarn build
+git add .
+git commit -m "deploying .."
+git push -u origin master
+google-chrome https://richardyantas.github.io/richardyantas/
+echo "changing the public_url to development format ... "
+export PUBLIC_URL=http://localhost:3000/
