c166ab2cc776d0d4a7dd04ae454e04daaa6dd1c4
richardyantas5 <richadyantas5@gmail.com>
2021-05-15 03:47:32 -0500

    sidebardata was changed according to navbar

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,86 +1,98 @@
 import React from "react";
 import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
-import * as IoIcons from "react-icons/io";
-import * as RiIcons from "react-icons/ri";
-
 export const SidebarData = [
   {
     title: "About Us",
     path: "/about-us",
-    icon: <AiIcons.AiFillHome />,
-    iconClosed: <RiIcons.RiArrowDownSFill />,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
-
+    iconClosed: "url(nav-button_right-arrow.png)",
+    iconOpened: "url(nav-button_bottom-arrow.png)",
+    icon: "url(nav-button.png)",
     subNav: [
       {
         title: "Our Aim",
         path: "/about-us/aim",
-        icon: <IoIcons.IoIosPaper />,
+        icon: "url(nav-button.png)",
       },
       {
         title: "Our Vision",
         path: "/about-us/vision",
-        icon: <IoIcons.IoIosPaper />,
+        icon: "url(nav-button.png)",
       },
     ],
   },
   {
     title: "Projects",
     path: "/projects",
-    icon: <IoIcons.IoIosPaper />,
-    iconClosed: <RiIcons.RiArrowDownSFill />,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
-
+    iconClosed: "url(nav-button_right-arrow.png)",
+    iconOpened: "url(nav-button_bottom-arrow.png)",
+    icon: "url(nav-button.png)",
     subNav: [
       {
         title: "projects 1",
-        path: "/services/services1",
-        icon: <IoIcons.IoIosPaper />,
+        path: "/projects/projects1",
+        icon: "url(nav-button.png)",
         cName: "sub-nav",
       },
       {
         title: "projects 2",
-        path: "/services/services2",
-        icon: <IoIcons.IoIosPaper />,
+        path: "/projects/projects2",
+        icon: "url(nav-button.png)",
         cName: "sub-nav",
       },
       {
         title: "projects 3",
-        path: "/services/services3",
-        icon: <IoIcons.IoIosPaper />,
+        path: "/projects/projects3",
+        icon: "url(nav-button.png)",
       },
     ],
+  },
+  {
+    title: "Backend",
+    path: "/backend",
+    iconClosed: "url(nav-button_right-arrow.png)",
+    iconOpened: "url(nav-button_bottom-arrow.png)",
+    icon: "url(nav-button.png)",
+  },
+  {
+    title: "Machine learning",
+    path: "/machine-learning",
+    iconClosed: "url(nav-button_right-arrow.png)",
+    iconOpened: "url(nav-button_bottom-arrow.png)",
+    icon: "url(nav-button.png)",
+  },
+  {
+    title: "Data Engineering",
+    path: "/data-engineering",
+    iconClosed: "url(nav-button_right-arrow.png)",
+    iconOpened: "url(nav-button_bottom-arrow.png)",
+    icon: "url(nav-button.png)",
+  },
+  {
+    title: "Data Science",
+    path: "/data-engineering",
+    iconClosed: "url(nav-button_right-arrow.png)",
+    iconOpened: "url(nav-button_bottom-arrow.png)",
+    icon: "url(nav-button.png)",
+  },
+  {
+    title: "Cloud Computing",
+    path: "/cloud-compuing",
+    iconClosed: "url(nav-button_right-arrow.png)",
+    iconOpened: "url(nav-button_bottom-arrow.png)",
+    icon: "url(nav-button.png)",
+  },
+  {
+    title: "Algorithms&DS",
+    path: "/algorithms-datastructures",
+    iconClosed: "url(nav-button_right-arrow.png)",
+    iconOpened: "url(nav-button_bottom-arrow.png)",
+    icon: "url(nav-button.png)",
   },
   {
     title: "Contact",
     path: "/contact",
-    icon: <FaIcons.FaPhone />,
-  },
-  {
-    title: "Events",
-    path: "/events",
-    icon: <FaIcons.FaEnvelopeOpenText />,
-
-    iconClosed: <RiIcons.RiArrowDownSFill />,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
-
-    subNav: [
-      {
-        title: "Event 1",
-        path: "/events/events1",
-        icon: <IoIcons.IoIosPaper />,
-      },
-      {
-        title: "Event 2",
-        path: "/events/events2",
-        icon: <IoIcons.IoIosPaper />,
-      },
-    ],
-  },
-  {
-    title: "Support",
-    path: "/support",
-    icon: <IoIcons.IoMdHelpCircle />,
+    iconClosed: "url(nav-button_right-arrow.png)",
+    iconOpened: "url(nav-button_bottom-arrow.png)",
+    icon: "url(nav-button.png)",
   },
 ];
