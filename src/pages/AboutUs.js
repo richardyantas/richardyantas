f6c801af2d2c0d95e45d30cf9e675b64772bd1a2
richardyantas5 <richadyantas5@gmail.com>
2021-05-15 03:48:17 -0500

    about is added but by default has a temporal notionpage

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
+// import { NotionRenderer } from "react-notion";
+import { NotionRenderer } from "react-notion-x";
+
+const urlPage =
+  "https://notion-api.splitbee.io/v1/page/ed98a1529f1241a69a03fb0df7abbeb2";
 
 export const AboutUs = () => {
+  const [loading, setLoading] = useState(true);
+  const [blogData, setBlogData] = useState({});
+  async function componentDidMount() {
+    const blogDataFetched = await fetch(urlPage).then((res) => res.json());
+    setLoading(false);
+    setBlogData(blogDataFetched);
+  }
+  useEffect(() => {
+    // localStorage.setItem("urlPage", urlPage);
+    componentDidMount();
+  }, [urlPage]);
+
   return (
-    <div className="home">
+    <>
       <h1>GeeksforGeeks About us</h1>
-    </div>
+      {loading ? <h1>Loading...</h1> : <NotionRenderer blockMap={blogData} />}
+    </>
   );
 };
 
