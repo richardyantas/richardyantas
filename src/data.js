a8a630a4963618a1cb8bdfd4f98e22fccf3037b4
richardyantas5 <richadyantas5@gmail.com>
2021-05-17 11:44:12 -0500

    deploying ..

--- src/data.js
+++ src/data.js
@@ -1,13 +1,12 @@
-export const noExpandable = `/nav-button.png`;
-export const expandable = `/nav-button_right-arrow.png`;
-export const expanded = `/nav-button_bottom-arrow.png`;
+export const noExpandable = `./nav-button.png`;
+export const expandable = `./nav-button_right-arrow.png`;
+export const expanded = `./nav-button_bottom-arrow.png`;
 
 export const MENU = [
   {
     name: "Home",
     path: "/home",
-    url:
-      "https://notion-api.splitbee.io/v1/page/d53ab561c3bf4317b035ca9c08b60d7a",
+    url: "https://notion-api.splitbee.io/v1/page/d53ab561c3bf4317b035ca9c08b60d7a",
     children: [],
   },
   {
@@ -17,23 +16,20 @@ export const MENU = [
       {
         name: "Sthocastic Games",
         path: "/project1",
-        url:
-          "https://notion-api.splitbee.io/v1/page/55978965a31d4355b1190d0059189924",
+        url: "https://notion-api.splitbee.io/v1/page/55978965a31d4355b1190d0059189924",
         children: [],
       },
       {
         name: "Urban R. networks",
         path: "/project2",
-        url:
-          "https://notion-api.splitbee.io/v1/page/fb1b9e08f84842b39f014742343ccc4e",
+        url: "https://notion-api.splitbee.io/v1/page/fb1b9e08f84842b39f014742343ccc4e",
         children: [],
       },
       {
         name: "Text Recognizer",
         path: "/project2",
         children: [],
-        url:
-          "https://notion-api.splitbee.io/v1/page/9fa38b45c12647b68afec7587b944b20",
+        url: "https://notion-api.splitbee.io/v1/page/9fa38b45c12647b68afec7587b944b20",
       },
     ],
   },
@@ -47,20 +43,17 @@ export const MENU = [
           {
             name: "Docker Compose",
             children: [],
-            url:
-              "https://notion-api.splitbee.io/v1/page/802dcb67d2914440a6dcf6707912be1d",
+            url: "https://notion-api.splitbee.io/v1/page/802dcb67d2914440a6dcf6707912be1d",
           },
           {
             name: "AWS Lambda",
             children: [],
-            url:
-              "https://notion-api.splitbee.io/v1/page/0fff2ffd25274e318a31df2063469540",
+            url: "https://notion-api.splitbee.io/v1/page/0fff2ffd25274e318a31df2063469540",
           },
           {
             name: "AWS EC2",
             children: [],
-            url:
-              "https://notion-api.splitbee.io/v1/page/4190b0d66c2b4ed5b10f6406a46eb961",
+            url: "https://notion-api.splitbee.io/v1/page/4190b0d66c2b4ed5b10f6406a46eb961",
           },
         ],
       },
@@ -70,8 +63,7 @@ export const MENU = [
           {
             name: "Pytorch Tabular",
             children: [],
-            url:
-              "https://notion-api.splitbee.io/v1/page/90608dd258534f378f6948ba9183746e",
+            url: "https://notion-api.splitbee.io/v1/page/90608dd258534f378f6948ba9183746e",
           },
         ],
       },
@@ -81,8 +73,7 @@ export const MENU = [
           {
             name: "Exploratory DA.",
             children: [],
-            url:
-              "https://notion-api.splitbee.io/v1/page/d4e777953c86444aad508e3f038992eb",
+            url: "https://notion-api.splitbee.io/v1/page/d4e777953c86444aad508e3f038992eb",
           },
         ],
       },
@@ -92,14 +83,12 @@ export const MENU = [
           {
             name: "Flask API",
             children: [],
-            url:
-              "https://notion-api.splitbee.io/v1/page/2aa082d5278342689c4be8c8570a616f",
+            url: "https://notion-api.splitbee.io/v1/page/2aa082d5278342689c4be8c8570a616f",
           },
           {
             name: "FastAPI",
             children: [],
-            url:
-              "https://notion-api.splitbee.io/v1/page/503b2424eb3943668f01e05ab44d013c",
+            url: "https://notion-api.splitbee.io/v1/page/503b2424eb3943668f01e05ab44d013c",
           },
         ],
       },
@@ -112,8 +101,7 @@ export const MENU = [
       {
         name: "Statistics",
         children: [],
-        url:
-          "https://notion-api.splitbee.io/v1/page/40e08fa308694e9b8edf776233d77f0e",
+        url: "https://notion-api.splitbee.io/v1/page/40e08fa308694e9b8edf776233d77f0e",
       },
       {
         name: "Economy",
@@ -121,34 +109,29 @@ export const MENU = [
           {
             name: "Macroeconomics",
             children: [],
-            url:
-              "https://notion-api.splitbee.io/v1/page/6cba99f396124b6181968a7f897271d8",
+            url: "https://notion-api.splitbee.io/v1/page/6cba99f396124b6181968a7f897271d8",
           },
         ],
       },
       {
         name: "Sociology",
         children: [],
-        url:
-          "https://notion-api.splitbee.io/v1/page/e85da77cbce54043acdb701002493bc0",
+        url: "https://notion-api.splitbee.io/v1/page/e85da77cbce54043acdb701002493bc0",
       },
       {
         name: "Linguistics",
         children: [],
-        url:
-          "https://notion-api.splitbee.io/v1/page/5b4413eb0bd745d28d883329d38575d6",
+        url: "https://notion-api.splitbee.io/v1/page/5b4413eb0bd745d28d883329d38575d6",
       },
       {
         name: "Biology",
         children: [],
-        url:
-          "https://notion-api.splitbee.io/v1/page/9848671446b0484bb46a104e616f7740",
+        url: "https://notion-api.splitbee.io/v1/page/9848671446b0484bb46a104e616f7740",
       },
       {
         name: "Robotics",
         children: [],
-        url:
-          "https://notion-api.splitbee.io/v1/page/8a964af37e6949fcaefef8699912a7ad",
+        url: "https://notion-api.splitbee.io/v1/page/8a964af37e6949fcaefef8699912a7ad",
       },
     ],
   },
@@ -156,8 +139,7 @@ export const MENU = [
     name: "Papers",
     path: "/papers",
     children: [],
-    url:
-      "https://notion-api.splitbee.io/v1/page/ed98a1529f1241a69a03fb0df7abbeb2",
+    url: "https://notion-api.splitbee.io/v1/page/ed98a1529f1241a69a03fb0df7abbeb2",
   },
   {
     name: "Books",
@@ -165,14 +147,12 @@ export const MENU = [
     children: [
       {
         name: "Deep Learning GF.",
-        url:
-          "https://notion-api.splitbee.io/v1/page/ed98a1529f1241a69a03fb0df7abbeb2",
+        url: "https://notion-api.splitbee.io/v1/page/ed98a1529f1241a69a03fb0df7abbeb2",
         children: [],
       },
       {
         name: "Data Science",
-        url:
-          "https://notion-api.splitbee.io/v1/page/ed98a1529f1241a69a03fb0df7abbeb2",
+        url: "https://notion-api.splitbee.io/v1/page/ed98a1529f1241a69a03fb0df7abbeb2",
         children: [],
       },
     ],
@@ -181,14 +161,12 @@ export const MENU = [
     name: "About",
     path: "/about",
     children: [],
-    url:
-      "https://notion-api.splitbee.io/v1/page/d53ab561c3bf4317b035ca9c08b60d7a",
+    url: "https://notion-api.splitbee.io/v1/page/d53ab561c3bf4317b035ca9c08b60d7a",
   },
   {
     name: "Contact",
     path: "contact",
     children: [],
-    url:
-      "https://notion-api.splitbee.io/v1/page/39e7102c299441b5a95e79eb163e0ed3",
+    url: "https://notion-api.splitbee.io/v1/page/39e7102c299441b5a95e79eb163e0ed3",
   },
 ];
