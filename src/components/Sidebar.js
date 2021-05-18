60a58f44ae7a0fbe7e9cd59c5dfe24b8c8d9ec95
richardyantas5 <richadyantas5@gmail.com>
2021-05-18 18:16:27 -0500

    deploying ..

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -30,20 +30,20 @@ const Sidebar = () => {
               href="https://www.amazon.com/Deep-Learning-Adaptive-Computation-Machine/dp/0262035618"
               target="_blank"
             >
-              <img src="/deeplearning.jpg" className="book" alt="W3Schools" />
+              <img src="./deeplearning.jpg" className="book" alt="W3Schools" />
             </a>
             <a
               href="https://www.amazon.com/Python-Data-Science-Handbook-Essential/dp/1491912057"
               target="_blank"
             >
-              <img src="/datascience.jpg" className="book" alt="W3Schools" />
+              <img src="./datascience.jpg" className="book" alt="W3Schools" />
             </a>
             <a
               href="https://www.amazon.com/-/es/Himanshu-Singh/dp/1484262212"
               target="_blank"
             >
               <img
-                src="/machinelearning.jpg"
+                src="./machinelearning.jpg"
                 className="book"
                 alt="W3Schools"
               />
