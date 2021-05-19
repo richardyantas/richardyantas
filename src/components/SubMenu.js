3ee4c60bf91722016143eb47e7b151e7b9ff0902
richardyantas5 <richadyantas5@gmail.com>
2021-05-18 20:58:39 -0500

    changing from ./ to /

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -18,9 +18,9 @@ const SidebarLink = styled(Link)`
   background-image: ${({ subnav, item }) =>
     item.subNav.length > 0
       ? subnav
-        ? "url(./nav-button_bottom-arrow.png)"
-        : "url(./nav-button_right-arrow.png)"
-      : "url(./nav-button.png)"};
+        ? "url(/nav-button_bottom-arrow.png)"
+        : "url(/nav-button_right-arrow.png)"
+      : "url(/nav-button.png)"};
   &:hover {
     color: hsl(225deg 33% 60%);
     cursor: pointer;
@@ -48,7 +48,7 @@ const DropdownLink = styled(Link)`
   background-repeat: no-repeat;
   background-size: 164px 31px;
   vertical-align: middle;
-  background-image: url(./nav-button.png);
+  background-image: url(/nav-button.png);
   &:hover {
     color: hsl(225deg 33% 60%);
     cursor: pointer;
@@ -70,7 +70,7 @@ const SubMenu = ({ item }) => {
         <div>
           <SidebarLabel>{item.title}</SidebarLabel>
         </div>
-        <img src="./nav-button_bottom-arrow.png" style={{ display: `none` }} />
+        <img src="/nav-button_bottom-arrow.png" style={{ display: `none` }} />
       </SidebarLink>
       {subnav &&
         item.subNav.map((item, index) => {
